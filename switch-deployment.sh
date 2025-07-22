a88eb04ff65d2025b22d1ccdebc51ac9b718dc93
pissedoffcitizen <216899182+pissedoffcitizen@users.noreply.github.com>
2025-07-22 10:42:46 -0600

    Update switch-deployment to maintain a test cloudfront deployment

--- switch-deployment.sh
+++ switch-deployment.sh
@@ -30,6 +30,7 @@ fi
 # CloudFront distribution IDs
 DISTRIBUTION_1="E33KLI11KRM1QA"
 DISTRIBUTION_2="E2MZLE77EVKTB"
+DISTRIBUTION_TEST="E3FIR5Y0R5OC5I"
 
 # Function to update CloudFront distribution
 update_distribution() {
@@ -55,11 +56,44 @@ update_distribution() {
   echo "Updated CloudFront distribution ($DISTRIBUTION_ID) to use origin: $NEW_ORIGIN"
 }
 
+# Function to update test distribution to the origin NOT currently in production
+update_distribution_invert() {
+  local DISTRIBUTION_ID=$1
+  local CONFIG_JSON="config_$DISTRIBUTION_ID.json"
+
+  # Determine the inverse origin
+  if [ "$POLISCORE_DEPLOYMENT_NUMBER" -eq 1 ]; then
+    INVERT_ORIGIN="poliscore-website2.s3-website-us-east-1.amazonaws.com"
+  else
+    INVERT_ORIGIN="poliscore-website1.s3-website-us-east-1.amazonaws.com"
+  fi
+
+  # Get current config
+  aws cloudfront get-distribution-config --id "$DISTRIBUTION_ID" > "$CONFIG_JSON"
+
+  # Extract ETag
+  ETAG=$(jq -r '.ETag' "$CONFIG_JSON")
+
+  # Modify origin
+  jq --arg NEW_ORIGIN "$INVERT_ORIGIN" '.DistributionConfig.Origins.Items[0].DomainName = $NEW_ORIGIN | .DistributionConfig' "$CONFIG_JSON" > "final_$CONFIG_JSON"
+
+  # Update distribution
+  aws cloudfront update-distribution --id "$DISTRIBUTION_ID" --if-match "$ETAG" --distribution-config file://final_$CONFIG_JSON
+
+  # Cleanup
+  rm "$CONFIG_JSON" "final_$CONFIG_JSON"
+
+  echo "Updated TEST CloudFront distribution ($DISTRIBUTION_ID) to use origin: $INVERT_ORIGIN"
+}
+
+
 # Update both distributions
 update_distribution "$DISTRIBUTION_1" "poliscore.us"
 update_distribution "$DISTRIBUTION_2" "www.poliscore.us"
+update_distribution_invert "$DISTRIBUTION_TEST"
 
 ./set-deployment-target.sh
 
 echo "CloudFront distributions updated successfully."
+echo "You may continue development on the test distribution at - d1q121pqch444h.cloudfront.net"
 
